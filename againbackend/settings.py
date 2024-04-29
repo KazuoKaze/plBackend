8d78c7e2f4b547144cd798c6754e9a07e9b5c783
Kazuo <kazuokaze00@gmail.com>
2024-04-29 16:30:44 +0530

    Static Commit

--- againbackend/settings.py
+++ againbackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,12 +133,18 @@ CART_SESSION_ID = 'cart'
 
 STATIC_URL = "/static/"
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media/"
 ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
 
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
